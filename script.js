03bb6e56b676fe7954a3d6afefee42a5218579b1
omar <omar@salamanderu.com>
2024-11-05 14:30:03 +0100

    iframe

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var About = document.getElementById("about");
 var Contact = document.getElementById("contact");
 var Projects = document.getElementById("projects");
 var Jupiter = document.getElementById("jupiter");
+var TreeFlow = document.getElementById("treeflow");
+var WFC = document.getElementById("wfc");
 
 
 var tabAbout = document.getElementById("tab-about");
@@ -18,112 +20,150 @@ var current = document.getElementsByClassName("current");
 const searchBar = document.getElementsByClassName("console");
 
 var results = [
-    ["education", 1],
-    ["experience", 2],
-    ["skills", 3],
-    ["hobbies", 4],
-    ["about", 5],
-    ["contact", 6],
-    ["projects", 7],
-    ["jupiter", 8],
+  ["education", 1],
+  ["experience", 2],
+  ["skills", 3],
+  ["hobbies", 4],
+  ["about", 5],
+  ["contact", 6],
+  ["projects", 7],
+  ["jupiter", 8],
+  ["treeflow", 9],
+  ["wfc", 10],
 ];
 
 var shownTabs = [1, 2, 3, 4];
 
+function showProject() {
+  console.log("called");
+  const projectNames = ["jupiter", "treeflow", "wfc"];
+  projectNames.forEach(projectName => {
+    let projectDiv = document.getElementById(projectName);
+    let iframe = document.getElementById(`iframe${projectName}`);
+    console.log(projectDiv.classList);
+    console.log(iframe);
+
+    if (projectDiv.classList.contains('current')) {
+      console.log("in if");
+      iframe.src = `https://owabbi.github.io/projects/${projectName.toLowerCase()}/index.html`;
+    } else {
+      iframe.src = "";
+    }
+  });
+}
+
 
 function show(part) {
-    switch (part) {
-        case 1:
-            current[0].classList.remove('current');
-            Education.classList.add('current');
-            searchBar[0].value = "education";
-            break;
-        case 2:
-            current[0].classList.remove('current');
-            Experience.classList.add('current');
-            searchBar[0].value = "experience";
-            break;
-        case 3:
-            current[0].classList.remove('current');
-            Skills.classList.add('current');
-            searchBar[0].value = "skills";
-            break;
-        case 4:
-            current[0].classList.remove('current');
-            Hobbies.classList.add('current');
-            searchBar[0].value = "hobbies";
-            break;
-        case 5:
-            current[0].classList.remove('current');
-            About.classList.add('current');
-            searchBar[0].value = "about";
-            break;
-        case 6:
-            current[0].classList.remove('current');
-            Contact.classList.add('current');
-            searchBar[0].value = "contact";
-            break;
-        case 7:
-            current[0].classList.remove('current');
-            Projects.classList.add('current');
-            searchBar[0].value = "projects";
-            break;
-        case 8:
-            current[0].classList.remove('current');
-            Jupiter.classList.add('current');
-            searchBar[0].value = "jupiter";
-            break;
+  switch (part) {
+    case 1:
+      current[0].classList.remove('current');
+      Education.classList.add('current');
+      searchBar[0].value = "education";
+      break;
+    case 2:
+      current[0].classList.remove('current');
+      Experience.classList.add('current');
+      searchBar[0].value = "experience";
+      break;
+    case 3:
+      current[0].classList.remove('current');
+      Skills.classList.add('current');
+      searchBar[0].value = "skills";
+      break;
+    case 4:
+      current[0].classList.remove('current');
+      Hobbies.classList.add('current');
+      searchBar[0].value = "hobbies";
+      break;
+    case 5:
+      current[0].classList.remove('current');
+      About.classList.add('current');
+      searchBar[0].value = "about";
+      break;
+    case 6:
+      current[0].classList.remove('current');
+      Contact.classList.add('current');
+      searchBar[0].value = "contact";
+      break;
+    case 7:
+      current[0].classList.remove('current');
+      Projects.classList.add('current');
+      searchBar[0].value = "projects";
+      break;
+    case 8:
+      current[0].classList.remove('current');
+      Jupiter.classList.add('current');
+      searchBar[0].value = "jupiter";
+      break;
+    case 9:
+      current[0].classList.remove('current');
+      TreeFlow.classList.add('current');
+      searchBar[0].value = "treeflow";
+      break;
+    case 10:
+      current[0].classList.remove('current');
+      WFC.classList.add('current');
+      searchBar[0].value = "wfc";
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+
+  }
+  showProject();
 
 }
 
 function activateTab(tabID) {
-    switch (tabID) {
-        case 5:
-            tabAbout.classList.remove('hidden-tab');
-            break;
-        case 6:
-            tabContact.classList.remove('hidden-tab');
-            break;
-        case 8:
-            tabJupiter.classList.remove('hidden-tab');
-            break;
+  switch (tabID) {
+    case 5:
+      tabAbout.classList.remove('hidden-tab');
+      break;
+    case 6:
+      tabContact.classList.remove('hidden-tab');
+      break;
+    case 8:
+      tabJupiter.classList.remove('hidden-tab');
+      break;
+    case 9:
+      tabJupiter.classList.remove('hidden-tab');
+      break;
+    case 10:
+      tabJupiter.classList.remove('hidden-tab');
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 function Search(item) {
-    console.log(item);
-    console.table(shownTabs);
-    for (let i = 0; i < results.length; i++) {
-        const elem = results[i];
-        console.log("Checking for " + elem[0]);
-        if (item == elem[0]) {
-            show(elem[1]);
-            const found = shownTabs.find(element => element == elem[1]);
-            if (found == undefined) {
-                activateTab(elem[1]);
-            }
-        }
-
+  console.log(item);
+  console.table(shownTabs);
+  for (let i = 0; i < results.length; i++) {
+    const elem = results[i];
+    console.log("Checking for " + elem[0]);
+    if (item == elem[0]) {
+      show(elem[1]);
+      const found = shownTabs.find(element => element == elem[1]);
+      if (found == undefined) {
+        activateTab(elem[1]);
+      }
     }
+
+  }
 }
 
 function activate() {
-    console.log("ACTIVATED");
+  console.log("ACTIVATED");
 
 }
 
 function UnlockAll(params) {
-    console.log("Unlock all");
-    for (let index = 0; index < 9; index++) {
-        activateTab(index);
-    }
+  console.log("Unlock all");
+  for (let index = 0; index < 9; index++) {
+    activateTab(index);
+  }
 }
 
 
@@ -211,16 +251,16 @@ function countAliveNeighbors(row, col) {
 
 // Function to start the game
 function startGame() {
-    initializeGrid(); // Initialize the grid with random alive cells
-    displayGrid(); // Display the initial grid
-  
-    // Start the game loop
-    setInterval(() => {
-      updateGrid(); // Update the grid based on the rules of the Game of Life
-      displayGrid(); // Display the updated grid
-    }, 200); // Adjust the interval (in milliseconds) to control the speed of the game
-  }
-  
-  // Call the startGame function to start the game
-  startGame();
-  
+  initializeGrid(); // Initialize the grid with random alive cells
+  displayGrid(); // Display the initial grid
+
+  // Start the game loop
+  setInterval(() => {
+    updateGrid(); // Update the grid based on the rules of the Game of Life
+    displayGrid(); // Display the updated grid
+  }, 200); // Adjust the interval (in milliseconds) to control the speed of the game
+}
+
+// Call the startGame function to start the game
+startGame();
+
